f85cccaf0ae6e128fa1703af19dcadcab7a1f9ce
Solar <poyas_oriona@rambler.ru>
2023-02-24 11:52:15 +0300

    Bobka

---
+++ app/src/main/java/com/example/anew/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.anew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.anew.R.*
+
+class MainActivity() : AppCompatActivity() {
+    private var state = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+        val nameAisha = findViewById<TextView>(id.text_aisha)
+        val foto = findViewById<ImageView>(id.foto)
+        val button: Button = findViewById(id.button_change_name)
+        button.setOnClickListener {
+            state = !state
+            if (state) {
+                foto.setImageResource(R.drawable.ic_bobka)
+                nameAisha.text = "Бобка красавчик!"
+                button.text = "Покажи мурку"
+            } else {
+                foto.setImageResource(R.drawable.ic_murka)
+                button.text = "Покажи бобку"
+                nameAisha.text = "Мурка зверь!"
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
