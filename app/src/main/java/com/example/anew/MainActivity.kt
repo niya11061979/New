8212445599916fe1c1008663bb41862d4280c3e8
Solar <poyas_oriona@rambler.ru>
2023-02-24 14:34:00 +0300

    Creat_final

--- app/src/main/java/com/example/anew/MainActivity.kt
+++ app/src/main/java/com/example/anew/MainActivity.kt
@@ -8,28 +8,39 @@ import android.widget.TextView
 import com.example.anew.R.*
 
 class MainActivity() : AppCompatActivity() {
-    private var count:Int= 1
-    private val bibka=3
+    private var count: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
         val nameAisha = findViewById<TextView>(id.text_aisha)
         val foto = findViewById<ImageView>(id.foto)
         val button: Button = findViewById(id.button_change_name)
-        button.setOnClickListener {
 
-                 if (count==1) {
-                    foto.setImageResource(drawable.ic_bobka)
-                    nameAisha.text = "Бобка красавчик!"
-                    button.text = "Покажи мурку"
-                } else {
-                    foto.setImageResource(drawable.ic_murka)
-                    button.text = "Покажи бобку"
-                    nameAisha.text = "Мурка зверь!"
-                }
+        button.setOnClickListener {
+            count += 1
+            if (count == 4) count = 1
+            if (count == 1) {
+                foto.setImageResource(drawable.ic_bobka)
+                nameAisha.text = "Бобка красавчик!"
+                button.text = "Покажи мурку"
+            }
+
+
+            if (count == 2) {
+                foto.setImageResource(drawable.ic_murka)
+                button.text = "покажи бибку"
+                nameAisha.text = "Мурка зверь!"
+            }
+
+            if (count == 3) {
+                foto.setImageResource(drawable.ic_bibka)
+                button.text = "покажи бобку"
+                nameAisha.text = "Бибка красавица!"
+
             }
         }
     }
+}
 
 
 
