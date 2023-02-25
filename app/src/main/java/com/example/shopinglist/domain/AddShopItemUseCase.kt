729db4c5b45ffb35fc0996edfa0083038ec1ff62
Solar <poyas_oriona@rambler.ru>
2023-02-25 20:06:13 +0300

    Create Shop app

--- app/src/main/java/com/example/shopinglist/domain/AddShopItemUseCase.kt
+++ app/src/main/java/com/example/shopinglist/domain/AddShopItemUseCase.kt
@@ -1,7 +1,5 @@
 package com.example.shopinglist.domain
 
 class AddShopItemUseCase(private val repository: ShopListRepository) {
-    fun addShopItem(shopItem: ShopItem) {
-    return repository.addShopItem(shopItem)
-    }
+    fun addShopItem(shopItem: ShopItem) { return repository.addShopItem(shopItem) }
 }
